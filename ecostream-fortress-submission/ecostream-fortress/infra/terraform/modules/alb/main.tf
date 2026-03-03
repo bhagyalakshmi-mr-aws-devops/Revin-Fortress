d6de0593c23cfe1a47574d4642e483fe31ebd0ca
BHAGYALAKSHMI MR <bhagyalakshmiaws@gmail.com>
2026-03-03 21:55:39 +0530

    Update main.tf

--- ecostream-fortress-submission/ecostream-fortress/infra/terraform/modules/alb/main.tf
+++ ecostream-fortress-submission/ecostream-fortress/infra/terraform/modules/alb/main.tf
@@ -2,3 +2,43 @@
 # - IAM policy for controller
 # - IRSA service account
 # - (Helm install can be done via GitOps or pipeline)
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = var.name
+  }
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.name}-igw"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnets)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnets[count.index]
+  map_public_ip_on_launch = true
+  availability_zone       = var.azs[count.index]
+
+  tags = {
+    Name = "${var.name}-public-${count.index}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = var.azs[count.index]
+
+  tags = {
+    Name = "${var.name}-private-${count.index}"
+  }
+}
